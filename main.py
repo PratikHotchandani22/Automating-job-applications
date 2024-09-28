c4117ec650ba6fb75ab8f3a8b84cd422855d23b8
ProjectKingsNLP <pratikhotchandani22@gmail.com>
2024-09-28 12:16:03 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from get_job_details import main_get_job_link
+
+def main():
+    print("Welcome to the python program that automates applyinh to jobs!!")
+    print("Called 01 file")
+    main_get_job_link()
+
+
+
+main()
