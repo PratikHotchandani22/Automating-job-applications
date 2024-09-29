c04254061562320aa2f1d0a4dc19f9488095f4e7
ProjectKingsNLP <pratikhotchandani22@gmail.com>
2024-09-28 20:37:49 -0400

    Scrapping job links to get job description details

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from get_job_details import main_get_job_link
+#from get_job_details import main_get_job_link, call_reader_api
+from get_job_details_crawl4ai import main_get_job_link, extract_job_description, extract_job_details
 
-def main():
+import asyncio
+
+
+async def main():
     print("Welcome to the python program that automates applyinh to jobs!!")
     print("Called 01 file")
-    main_get_job_link()
+    job_link = main_get_job_link()
+    await extract_job_description(job_link)
+    await extract_job_details(job_link)
 
 
 
-main()
+# Ensure the event loop is run properly
+if __name__ == "__main__":
+    asyncio.run(main())  # Run the async main function
